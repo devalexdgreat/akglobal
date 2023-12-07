9d5427bae988782e786f6b56547251b3a812e363
devalex_dgreat <opponamer@gmail.com>
2023-12-08 00:03:14 +0100

    done last and never coming back

--- app/contact/page.js
+++ app/contact/page.js
@@ -5,22 +5,43 @@ import { Switch } from '@headlessui/react'
 import HomeNav from '@/components/HomeNav';
 import React from 'react';
 import emailjs from '@emailjs/browser';
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+import Swal from 'sweetalert2';
+import { useRouter } from 'next/navigation';
 
 export default function Contact() {
     const form = useRef();
+    const emlInp = useRef();
+    const namInp = useRef();
+    const msgInp = useRef();
+    const router = useRouter();
+
+    const resetInputs = () => {
+        emlInp.current.value = "";
+        namInp.current.value = "";
+        msgInp.current.value = "";
+    };
 
     const sendEmail = (e) => {
         e.preventDefault();
     
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        emailjs.sendForm('service_oxsdk4b', 'template_jymofqj', form.current, 'dL1sKw6bMfycAJOwT')
           .then((result) => {
-              console.log(result.text);
+            console.log(result.text);
+            Swal.fire({
+                icon: "success",
+                title: "Message Sent",
+                showConfirmButton: false,
+                timer: 1500
+              });
+            resetInputs();
           }, (error) => {
               console.log(error.text);
+              Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Something went wrong!"
+              });
+              resetInputs();
           });
       };
 
@@ -31,42 +52,26 @@ export default function Contact() {
         <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
             <p className="mt-2 text-lg leading-8 text-gray-600">
-            Drop a Message to Reach out to us!
+                Drop a Message to Reach out to us!
             </p>
         </div>
 
 
         <form ref={form} onSubmit={sendEmail} className="mx-auto mt-16 max-w-xl sm:mt-20">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            <div>
-                <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                First name
+            <div className="sm:col-span-2">
+                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                    Full name
                 </label>
                 <div className="mt-2.5">
-                <input
+                <input ref={namInp}
                     type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 
-                    ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset 
-                    focus:ring-blue-500 sm:text-sm sm:leading-6 outline-none"
-                />
-                </div>
-            </div>
-            <div>
-                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                Last name
-                </label>
-                <div className="mt-2.5">
-                <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
+                    name="name_id"
+                    id="name_id"
+                    autoComplete="name"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm 
                     ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
-                    focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 outline-none"
+                    focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 outline-none" required
                 />
                 </div>
             </div>
@@ -75,14 +80,14 @@ export default function Contact() {
                 Email
                 </label>
                 <div className="mt-2.5">
-                <input
+                <input ref={emlInp}
                     type="email"
-                    name="email"
-                    id="email"
+                    name="email_id"
+                    id="email_id"
                     autoComplete="email"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm 
                     ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
-                    focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 outline-none"
+                    focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 outline-none" required
                 />
                 </div>
             </div>
@@ -91,14 +96,14 @@ export default function Contact() {
                 Message
                 </label>
                 <div className="mt-2.5">
-                <textarea
+                <textarea ref={msgInp}
                     name="message"
                     id="message"
                     rows={4}
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm 
                     ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
                     focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6 outline-none"
-                    defaultValue={''}
+                    defaultValue={''} required
                 />
                 </div>
             </div>
